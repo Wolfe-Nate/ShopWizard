f09a1a05f9c617f5c135655e03f19be91f9acc6a
Dylan <dylan.vonwald@gmail.com>
2022-07-14 14:21:58 -0600

    started resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,11 @@
-// fdsfdsfsd
+const { Category, Comment, Game, Item, User } = require("../models");
+
+const resolvers = {
+  Query: {
+    user: async () => {
+      return User.find({});
+    },
+  },
+};
+
+module.exports = resolvers;
