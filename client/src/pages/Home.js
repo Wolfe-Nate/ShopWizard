7ddfa0a51e090956b65fdab05e7a80862661ccb2
Luke Renoe <lukerenoe@gmail.com>
2022-07-15 10:59:50 -0600

    styling, homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,20 @@
-// dsdfsds
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Header from '../components/Header';
+// import Card from './components/Card';
+
+function Home() {
+    const message = '';
+    return (
+      <div className="rpgui-container.framed">
+        <h1 className='rpgui-content h1'>Welcome, weary traveler...</h1>
+        <div className='rpgui-content a' >
+        Login
+        </div>
+      </div>
+      
+    );
+  }
+  
+  export default Home;
