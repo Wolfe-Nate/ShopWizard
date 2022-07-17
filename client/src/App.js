54d631ae5e54172c96285986f3597166b17b48e3
Luke Renoe <lukerenoe@gmail.com>
2022-07-17 16:40:14 -0600

    got components to render on screen

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,16 @@ import Login from './pages/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from '../src/pages/Home';
-import Signup from './pages/Signup';
+// import Signup from './pages/Signup';
+import "./styles/style.css"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="rpgui-content h1">
       <Header></Header>
-      <BrowserRouter>
-      <Routes>
-<Route
-path='/home' element={<Home/>}
-/>
-<Route
-path='/login' element={<Login/>}
-/>
-<Route
-path='/signup' element={<Signup/>}
-/>
-      </Routes>
-      </BrowserRouter>
+      <Home></Home>
+      <Login></Login>
       <Footer></Footer>
     </div>
   );
