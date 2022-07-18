1774d489914cd2153674b3eea192909f15fc6a7f
Nate Wolfe <wolfe.nate26@gmail.com>
2022-07-18 16:54:02 -0600

    figuring out additem mutation

--- client/src/components/Additem.js
+++ client/src/components/Additem.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 
-import { ADD_ITEM } from "../../utils/mutations";
+import { ADD_ITEM } from "../utils/mutations";
 import Additem from "../pages/Additem";
 
 const AddItemForm = () => {
   const [formState, setFormState] = useState({
     itemName: "",
-    ItemPrice: "",
-    itemDesc: "",
-    gameName: "",
+    price: "",
+    description: "",
+    category: "",
+    $gameName: "",
   });
+  const [Additem, {error}] = useMutation(ADD_ITEM, {update(cache, {data: addItem}}) {}
 };
 
 export default Additem;
