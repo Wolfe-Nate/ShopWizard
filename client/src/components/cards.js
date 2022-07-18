65d1e098078787e7891a8a305bf8ce609da30060
J TAL <jjtalamonti@gmail.com>
2022-07-18 12:21:57 -0500

    Card data

--- client/src/components/cards.js
+++ client/src/components/cards.js
@@ -1,33 +1,41 @@
-const item = require("item");
 import React from 'react';
-
 import '../styles/style.css';
+// itemName
+// image
+// price
+// description
+// category
+// gameName
 
-function weaponCard() {
+const ItemCard = ({ item }) => {
+    if (!ItemCard.length) {
+        return <h3>No Items Yet</h3>;
+    }
     return (
         <div classNameName="rpgui-content" style="width:280px; height:415px;">
             <div className="rpgui-container framed rpgui-draggable" style="left:560px">
-                <h1>Item Name</h1>
+                { }
+                <h1>{item.itemName}</h1>
                 <hr className="golden" />
 
                 <div className="itemImage scimitar" style="margin-left: 25%;"></div>
 
                 <div className="rpgui-container framed-golden-2" style="margin-top: 5px; width:100%; height:90px; left:0px; top:0px;">
-                    <label>Item description.</label>
+                    <label>{item.description}.</label>
                 </div>
                 <hr className="golden" />
                 <div>
                     <div className="rpgui-icon coin hovertext" style="margin-left:25px; margin-right:25px;" data-hover="1000" >
-                        <label>1000</label>
+                        <label>{item.price}</label>
                     </div>
 
                     <div className="rpgui-icon sword hovertext" style="margin-left:25px; margin-right:25px;" data-hover="Weapon">
-                        <label>Weapon</label>
+                        <label>{item.category}</label>
 
                     </div>
                     <hr className="golden-2" />
                     <div style="margin-left:25%;">
-                        <p>Game Name</p>
+                        <p>{item.gameName}</p>
                     </div>
 
                 </div>
@@ -36,4 +44,4 @@ function weaponCard() {
     );
 }
 
-export default WeaponCard;
+export default Card;
