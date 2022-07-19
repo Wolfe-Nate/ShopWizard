939fd6dc64ff2887ae8b9e155e3e62538de3b32d
Nate Wolfe <wolfe.nate26@gmail.com>
2022-07-19 14:50:13 -0600

    add item tested and working

--- client/src/components/cards.js
+++ client/src/components/cards.js
@@ -39,108 +39,12 @@ const ItemCard = ({ items }) => {
               position: "relative",
               padding: "1rem",
             }}
-<<<<<<< HEAD
           >
             <Card item={item} currentUser={currentUser} />
           </div>
         ))}
     </div>
   );
-=======
-        >
-            {items &&
-                items.map((item) => (
-                    <div className="rpgui-container framed rpgui-draggable"
-                        style={{
-                            width: '280px',
-                            height: '520px',
-                            marginTop: "8rem",
-                            marginLeft: "3rem",
-                            display: "inline-block",
-                            position: "relative",
-                            padding: "1rem",
-
-                        }}
-                    >
-
-                        <div
-                            style={{
-                                padding: ".2rem"
-                            }}
-                        >
-                            <h1>{item.itemName}</h1>
-                        </div>
-                        <hr className="golden" />
-
-                        <div>
-                            <img src={item.image} alt={item.itemName} style={{ marginLeft: '25%', width: '120px', height: '120px', }}
-                            ></img>
-                        </div>
-
-                        <div className="rpgui-container framed-golden-2"
-                            style={{
-                                // marginTop: '5px',
-                                width: '100%',
-                                height: '90px',
-                                // left: '0px',
-                                // top: '0px',
-
-                            }}
-                        >
-
-                            <label>{item.description}.</label>
-                        </div>
-                        <hr className="golden" />
-
-                        <div className="rpgui-icon coin hovertext"
-                            style={{
-                                marginLeft: '25px',
-                                // marginRight: '25px',
-                                // data-hover= {item.price}, 
-                            }}
-                        >
-                            <label>{item.price}</label>
-                        </div>
-
-                        <div className="rpgui-icon sword hovertext"
-                            style={{
-                                marginLeft: '25px',
-                                // marginRight: '25px',
-                                // data-hover={item.category},
-                            }}
-                        >
-                            <label>{item.category}</label>
-                        </div>
-                        <hr className="golden-2" />
-
-                        <div className=''
-                        // style={{
-                        //     marginLeft: '25%',
-                        // }}
-                        >
-                            <p
-                            style={{
-                            fontSize: ".6rem",
-                            maxHeight: "5rem"
-                            }}
-                            >{item.gameName}</p>
-                        </div>
-                        <div>
-                            <button className="rpgui-button golden" type="button"
-                                style={{
-                                    marginLeft: "4%"
-                                }}>
-                                <p style={{
-                                    marginTop: "13px"
-                                }}>Purchase Item</p>
-
-                            </button>
-                        </div>
-                    </div>
-                ))}
-        </div>
-    );
->>>>>>> main
 };
 
 export default ItemCard;
